85609c6b8dd049c7dd192462bf0bee9b9b3dce8c
minhhoangyeu <hanhan.nguyengia@gmail.com>
2018-08-31 10:03:40 +0700

    The First Commit

---
+++ StafftravelManagement.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StafftravelManagement.Web.MvcApplication" Language="C#" %>
---
+++ StafftravelManagement.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StafftravelManagement.Web.Startup))]
+namespace StafftravelManagement.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
